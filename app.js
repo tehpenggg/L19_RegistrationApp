a39fc636ddada62d8698c7a566914d012ad3d9f1
tehpenggg <25003666@myrp.edu.sg>
2026-07-16 11:46:07 +0800

    Database Credentials Updated

--- app.js
+++ app.js
@@ -9,11 +9,22 @@ const flash = require('connect-flash');
 const app = express();
 
 // Database connection
+//const db = mysql.createConnection({
+//    host: 'localhost',
+//    user: 'root',
+//    password: 'seanseah007!',
+//    database: 'C237_usersdb'
+// });
+
+// [C237-030] Database connection to Azure MySQL Database
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'seanseah007!',
-    database: 'C237_usersdb'
+    host: 'c237-annie-mysql.mysql.database.azure.com',
+    user: 'c237_030',
+    password: 'c237030@2026!',
+    database: 'c237_030_regapp_ca2team2',
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 db.connect((err) => {
